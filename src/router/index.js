bb3f3045e96718a292d341d6793f339651a5e026
HaiND <haind@kaopiz.com>
2019-09-25 16:56:35 +0700

    fix router

---
+++ src/router/index.js
@@ -0,0 +1,64 @@
+/**
+ * Vue Router
+ *
+ * @library
+ *
+ * https://router.vuejs.org/en/
+ */
+
+// Lib imports
+import Vue from 'vue'
+import VueAnalytics from 'vue-analytics'
+import Router from 'vue-router'
+import Meta from 'vue-meta'
+
+// Routes
+
+Vue.use(Router)
+
+// Create a new router
+const router = new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/admin',
+      component: () => import('@/views/Admin.vue'),
+      children: [
+        {
+          path: 'dashboard',
+          component: () => import('@/views/Dashboard.vue'),
+        },
+        {
+          path: 'user-profile',
+          component: () => import('@/views/UserProfile.vue'),
+        },
+      ],
+    },
+  ],
+  scrollBehavior (to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    }
+    if (to.hash) {
+      return { selector: to.hash }
+    }
+    return { x: 0, y: 0 }
+  },
+})
+
+Vue.use(Meta)
+
+// Bootstrap Analytics
+// Set in .env
+// https://github.com/MatteoGabriele/vue-analytics
+if (process.env.GOOGLE_ANALYTICS) {
+  Vue.use(VueAnalytics, {
+    id: process.env.GOOGLE_ANALYTICS,
+    router,
+    autoTracking: {
+      page: process.env.NODE_ENV !== 'development',
+    },
+  })
+}
+
+export default router
---
+++ src/views/Admin.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <core-app-bar />
+
+    <core-drawer />
+
+    <core-view />
+
+    <core-footer />
+  </div>
+</template>
+
+<script>
+  export default {
+    components: {
+      CoreDrawer: () => import('@/components/core/Drawer'),
+      CoreFooter: () => import('@/components/core/Footer'),
+      CoreAppBar: () => import('@/components/core/AppBar'),
+      CoreView: () => import('@/components/core/View'),
+    },
+  }
+</script>
