3fc64049d1d10cf8843564ef0ee30b3a00f733d9
HaiND <haind@kaopiz.com>
2019-12-04 13:42:54 +0700

    qrcode

---
+++ src/views/PaymentPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div style="margin:auto">
+    <qrcode-vue :value="value" :size="size" level="H"></qrcode-vue>
+  </div>
+</template>
+<script>
+  import QrcodeVue from 'qrcode.vue'
+ 
+  export default {
+    data() {
+      return {
+        value: 'bitcoin:2NAr4rDnwhTfDnb9ziNpF5JLJTNLFfeoM97?amount=0.01',
+        size: 300,
+      }
+    },
+    components: {
+      QrcodeVue,
+    },
+  }
+</script> 
